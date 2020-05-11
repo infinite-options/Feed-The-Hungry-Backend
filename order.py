c2bcd06028dfd1d3cb428a7922626d189cfd6ee2
Pragya Singhal <40618413+pragya265@users.noreply.github.com>
2020-05-10 17:22:01 -0700

    Add files via upload

---
+++ order.py
@@ -0,0 +1,180 @@
+from flask import Flask, request, render_template
+from flask_restful import Resource, Api
+from flask_cors import CORS
+
+from werkzeug.exceptions import BadRequest, NotFound
+
+from dateutil.relativedelta import *
+from decimal import Decimal
+from datetime import datetime, date, timedelta
+from hashlib import sha512
+from math import ceil
+
+import decimal
+import sys
+import json
+import pymysql
+
+RDS_HOST = 'pm-mysqldb.cxjnrciilyjq.us-west-1.rds.amazonaws.com'
+RDS_PORT = 3306
+RDS_USER = 'admin'
+RDS_DB = 'feed_the_hungry'
+
+app = Flask(__name__)
+
+# Allow cross-origin resource sharing
+cors = CORS(app, resources={r'/api/*': {'origins': '*'}})
+
+# Set this to false when deploying to live application
+app.config['DEBUG'] = True
+
+# API
+api = Api(app)
+
+# Get RDS password from command line argument
+def RdsPw():
+    if len(sys.argv) == 2:
+        return str(sys.argv[1])
+    return ""
+
+# RDS PASSWORD
+# When deploying to Zappa, set RDS_PW equal to the password as a string
+# When pushing to GitHub, set RDS_PW equal to RdsPw()
+RDS_PW = 'prashant'
+# RDS_PW = RdsPw()
+
+
+getToday = lambda: datetime.strftime(date.today(), "%Y-%m-%d")
+getNow = lambda: datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
+
+# Connect to RDS
+def getRdsConn(RDS_PW):
+    global RDS_HOST
+    global RDS_PORT
+    global RDS_USER
+    global RDS_DB
+
+    print("Trying to connect to RDS...")
+    try:
+        conn = pymysql.connect(RDS_HOST,
+                               user=RDS_USER,
+                               port=RDS_PORT,
+                               passwd=RDS_PW,
+                               db=RDS_DB)
+        cur = conn.cursor()
+        print("Successfully connected to RDS.")
+        return [conn, cur]
+    except:
+        print("Could not connect to RDS.")
+        raise Exception("RDS Connection failed.")
+
+# Connect to MySQL database (API v2)
+def connect():
+    global RDS_PW
+    global RDS_HOST
+    global RDS_PORT
+    global RDS_USER
+    global RDS_DB
+
+    print("Trying to connect to RDS (API v2)...")
+    try:
+        conn = pymysql.connect( RDS_HOST,
+                                user=RDS_USER,
+                                port=RDS_PORT,
+                                passwd=RDS_PW,
+                                db=RDS_DB,
+                                cursorclass=pymysql.cursors.DictCursor)
+        print("Successfully connected to RDS. (API v2)")
+        return conn
+    except:
+        print("Could not connect to RDS. (API v2)")
+        raise Exception("RDS Connection failed. (API v2)")
+
+# Disconnect from MySQL database (API v2)
+def disconnect(conn):
+    try:
+        conn.close()
+        print("Successfully disconnected from MySQL database. (API v2)")
+    except:
+        print("Could not properly disconnect from MySQL database. (API v2)")
+        raise Exception("Failure disconnecting from MySQL database. (API v2)")
+
+# Serialize JSON
+def serializeResponse(response):
+    try:
+        for row in response:
+            for key in row:
+                if type(row[key]) is Decimal:
+                    row[key] = float(row[key])
+                elif type(row[key]) is date or type(row[key]) is datetime:
+                    row[key] = row[key].strftime("%Y-%m-%d")
+        return response
+    except:
+        raise Exception("Bad query JSON")
+
+# Execute an SQL command (API v2)
+# Set cmd parameter to 'get' or 'post'
+# Set conn parameter to connection object
+# OPTIONAL: Set skipSerialization to True to skip default JSON response serialization
+def execute(sql, cmd, conn, skipSerialization = False):
+    response = {}
+    try:
+        with conn.cursor() as cur:
+            cur.execute(sql)
+            if cmd is 'get':
+                result = cur.fetchall()
+                response['message'] = 'Successfully executed SQL query.'
+                # Return status code of 280 for successful GET request
+                response['code'] = 280
+                if not skipSerialization:
+                    result = serializeResponse(result)
+                response['result'] = result
+            elif cmd in 'post':
+                conn.commit()
+                response['message'] = 'Successfully committed SQL command.'
+                # Return status code of 281 for successful POST request
+                response['code'] = 281
+            else:
+                response['message'] = 'Request failed. Unknown or ambiguous instruction given for MySQL command.'
+                # Return status code of 480 for unknown HTTP method
+                response['code'] = 480
+    except:
+        response['message'] = 'Request failed, could not execute MySQL command.'
+        # Return status code of 490 for unsuccessful HTTP request
+        response['code'] = 490
+    finally:
+        response['sql'] = sql
+        return response
+
+# Close RDS connection
+def closeRdsConn(cur, conn):
+    try:
+        cur.close()
+        conn.close()
+        print("Successfully closed RDS connection.")
+    except:
+        print("Could not close RDS connection.")
+
+class Orders(Resource):
+    def post(self):
+        response = {}
+        items = {}
+
+        try:
+            conn = connect()
+
+            items = execute("""INSERT INTO feed_the_hungry.orders(order_id,user_id, foodbank_id, list, timestamp, status) 
+   VALUES ("900-000021", "200-000005","800-000006", '["500-000001", "500-000002", "500-000003", "500-000004", "500-000005", "500-000004", "500-000005"]',"05-10-2020 15:39:23", "pending")""", 'post', conn)
+            response['message'] = 'successful'
+            response['result'] = items
+
+            return response, 200
+        except:
+            raise BadRequest('Request failed, please try again later.')
+        finally:
+            disconnect(conn)
+
+api.add_resource(Orders, '/api/v2/add_order')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=4000)
